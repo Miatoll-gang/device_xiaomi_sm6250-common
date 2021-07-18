78d1a6898571b90f029544674d3c6b73b5915bc3
Alexander Winkowski <dereference23@outlook.com>
2021-07-18 15:55:45 +0200

    sm6250-common: Add framework compatibility matrix
    * Generated with https://gist.github.com/SebaUbuntu/ec053a00a9988eaea091fb1cc1f19324
    
    Change-Id: Ic40d19adb0d47a52c4b82eb71bb60ddaff76548d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += kpti=off
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
